a4d7831795a643d92f30390475d93c14a21738b6
Kevdagoat23 <90337345+Kevdagoat23@users.noreply.github.com>
2021-10-29 13:09:43 -0400

    3A v0.1
    Put three basic varables

--- .github/variable_practice.py
+++ .github/variable_practice.py
@@ -1,4 +1,8 @@
-# Variables Practice, Kevin Odom, 10/29/21, 12:48pm, Version 0.0
+# Variables Practice, Kevin Odom, 10/29/21, 12:59pm, Version 0.1
 
 
 # Basic Variables 
+
+my_int = -20 # Intergers, any +/- whole numbers and 0.
+my_float = 1.234567 # Floating Point, any +/- number with a decimal.
+my_string = "kev is da goat"

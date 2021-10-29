a0015159bce9bdf65950a5a0cb30da53042f3292
Kevdagoat23 <90337345+Kevdagoat23@users.noreply.github.com>
2021-10-29 13:22:05 -0400

    3A V.03
    Finished code

--- .github/variable_practice.py
+++ .github/variable_practice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Kevin Odom, 10/29/21, 1:06pm, Version 0.2
+# Variables Practice, Kevin Odom, 10/29/21, 1:20pm, Version 0.3
 
 
 # Basic Variables 
@@ -9,4 +9,13 @@ my_string = "kev is da goat"
 
 print(my_int)
 print(my_float)
+print(my_string)
+
+my_int = input("How many girls can you get in 2 hours? /n")
+print(my_int)
+
+my_float = input("What is your 40 yard dash time?/n")
+print(my_float)
+
+my_float = input("Please type your name./n")
 print(my_string)
